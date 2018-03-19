102d54044ad78f24aa87a2961df9e2337b93da25
ChandramohanG [aka-Mohan] <ganesanchandramohan@gmail.com>
2018-03-19 09:23:51 +0530

    Working on template and power shell scripts fot Infra generation...

--- wip_runningscripts.ps1
+++ wip_runningscripts.ps1
@@ -4,8 +4,18 @@ Login-AzureRmAccount
 
 Set-AzureRmContext -Subscription 'Windows Azure MSDN - Visual Studio Ultimate'
 
-$resourceGroupName = "RG_IoT"
+$resourceGroupName = "RG_IoTMonitor"
 $resourceGroupLocation = "West Europe"
+$IoTHubName = "IoTMonitoring"
+$inputName ="iotdata"
 
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 
+Set-AzureRmResourceGroup -Name $resourceGroupName -Tag @{Department="IoT"}
+
+New-AzureRmIotHub -ResourceGroupName $resourceGroupName -Name "iotmonitor" -SkuName "F1" -Location $resourceGroupLocation  
+
+
+Get-AzureRmIotHub -ResourceGroupName RG_IoTMonitor  
+
+Get-AzureRmStreamAnalyticsInput -ResourceGroupName RG_IoT
